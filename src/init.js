2f395108ccd887a7293e3633e66fc3f474f482ab
Jonathan Bradshaw <lavin@posteo.de>
2017-04-04 22:01:10 -0700

    Refactor code

--- src/init.js
+++ src/init.js
@@ -2,11 +2,8 @@ $(document).ready(function() {
   window.dancers = [];
 
   $('.addDancerButton').on('click', function(event) {
-
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-
     var dancerMakerFunction = window[dancerMakerFunctionName];
-
     var dancer = new dancerMakerFunction(
       $("body").height() / 1.75 * Math.random(),
       $("body").width() /1.1 * Math.random(),
@@ -17,17 +14,9 @@ $(document).ready(function() {
     $('body').append(dancer.$node);
   });
 
-
   $('.lineUpButton').on('click', function(event) {
     for(var i = 0; i < window.dancers.length; i++){
       window.dancers[i].lineUp();
     }
   });
-
-  var getDistance = function(point1, point2){
-    var xDiff = point2.left - point1.left;
-    var yDiff = point2.top - point1.top 
-
-    return Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2));
-  }
 });
--- src/shyDancer.js
+++ src/shyDancer.js
@@ -1,6 +1,18 @@
 var ShyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
   this.$node.addClass("shy");
+
+  this.$node.on('mouseover', function(){
+    
+    var tempX = $("body").width() * Math.random();
+    var tempY = $("body").height() * Math.random();
+
+    $(this).css({
+      left:tempX+"px",
+      top:tempY+"px",
+    });
+
+  });
 };
 
 ShyDancer.prototype = Object.create(Dancer.prototype);
@@ -8,10 +20,4 @@ ShyDancer.prototype.constructor = ShyDancer;
 
 ShyDancer.prototype.step = function() {
   Dancer.prototype.step.call(this);
-  this.$node.on('mouseover', function(){
-            $(this).css({
-                left:($("body").width() * Math.random())+"px",
-                top:($("body").height() * Math.random())+"px",
-            });
-        });
 };  
